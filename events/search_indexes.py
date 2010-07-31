1c143cd84eef64556f5ff15f8fd90c7269f7ebd9
Philipp Wassibauer <phil@philmac.lan>
2010-07-31 15:04:30 +0200

    folder rearranged

---
+++ events/search_indexes.py
@@ -0,0 +1,29 @@
+import datetime
+from haystack import indexes
+from haystack.sites import site
+from event.models import Event
+
+from haystack.sites import site
+#
+#class EventIndex(indexes.SearchIndex):
+#    text = indexes.CharField(document=True, use_template=True)
+#    title = indexes.CharField(model_attr='title')
+#    tease = indexes.CharField(model_attr='tease') 
+#    creator = indexes.CharField(model_attr='author')
+#
+#    created_at = indexes.DateTimeField(model_attr='created_at')
+#    updated_at = indexes.DateTimeField(model_attr='updated_at')
+#    start_date = indexes.DateTimeField(model_attr='start_date')
+#    end_date = indexes.DateTimeField(model_attr='end_date')
+#    
+#    event_type = indexes.IntegerField(model_attr='type')
+#
+#    tags = indexes.CharField(model_attr='tags')
+#    
+#    def get_query_set(self):
+#        return Event.objects.filter(status=2)
+#
+#
+#site.register(Event, EventIndex)
+#
+
